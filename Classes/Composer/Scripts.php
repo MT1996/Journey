6dd1e3a68030227a9e678febc198bf899a4a10f2
Marc Totzke <Marc.Totzke@t-online.de>
2020-04-23 17:20:12 +0200

    Wrong types used for Composer Scripts.php

--- Classes/Composer/Scripts.php
+++ Classes/Composer/Scripts.php
@@ -2,14 +2,14 @@
 
 namespace TheWorldsCMS\Journey\Composer;
 
-use Composer\Package\PackageInterface;
+use Composer\Installer\PackageEvent;
 use TheWorldsCMS\Utility\Utility;
 
 class Scripts {
-    public static function postUpdateAndInstallCommand(PackageInterface $event) {
+    public static function postUpdateAndInstallCommand(PackageEvent $event) {
         Utility::debug($event->getName());
     }
-    public static function postUpdateAndInstallPackage(PackageInterface $event) {
+    public static function postUpdateAndInstallPackage(PackageEvent $event) {
         Utility::debug($event->getName());
     }
 }
