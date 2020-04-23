150cf99144314b5a9fbe9ffae8321d39250c0f62
Marc Totzke <Marc.Totzke@t-online.de>
2020-04-23 17:40:17 +0200

    Trying something...

--- Classes/Composer/Scripts.php
+++ Classes/Composer/Scripts.php
@@ -3,13 +3,12 @@
 namespace TheWorldsCMS\Journey\Composer;
 
 use Composer\Installer\PackageEvent;
-use TheWorldsCMS\Utility\Utility;
 
 class Scripts {
     public static function postUpdateAndInstallCommand(PackageEvent $event) {
-        Utility::debug($event->getName());
+        TheWorldsCMS\Utility\debug($event->getName());
     }
     public static function postUpdateAndInstallPackage(PackageEvent $event) {
-        Utility::debug($event->getName());
+        TheWorldsCMS\Utility\debug($event->getName());
     }
 }
