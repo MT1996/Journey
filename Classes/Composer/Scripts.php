62f9173b1515a35819959c7226a3fae77f4e8ad4
Marc Totzke <Marc.Totzke@t-online.de>
2020-04-23 18:34:50 +0200

    Trying something...again

--- Classes/Composer/Scripts.php
+++ Classes/Composer/Scripts.php
@@ -7,9 +7,13 @@ use function TheWorldsCMS\Journey\Utility\debug;
 
 class Scripts {
     public static function postUpdateAndInstallCommand(PackageEvent $event) {
+        debug("------------------------------------------------------------------");
         debug($event->getName());
+        debug("------------------------------------------------------------------");
     }
     public static function postUpdateAndInstallPackage(PackageEvent $event) {
+        debug("------------------------------------------------------------------");
         debug($event->getName());
+        debug("------------------------------------------------------------------");
     }
 }
